f5910c90e0b3b9d8adb75b86d5480615757bb8e9
vinayak <vinsh752@gmail.com>
2022-09-12 17:55:53 +0530

    first comment in feature1

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.gitpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val arrList = ArrayList<Int>()
+
+        Log.d("1","1")
     }
 }
